208e230c111f2e289862c2ef2189e47cbe175346
Reza Sadeghzadeh <reza_sadeghzadeh01@yahoo.com>
2022-12-08 22:09:45 +0330

    fix: small opacity issue

--- components/About.tsx
+++ components/About.tsx
@@ -10,17 +10,17 @@ const About = () => {
           style={{
             boxShadow: "rgb(27 120 222 / 60%) 0px 0px 100px 60px",
           }}
-          className="absolute  rounded-full opacity-50 h-96 w-96 -left-48 -top-80  bg-brand-blueShadow/50"
+          className="absolute pointer-events-none rounded-full opacity-50 h-96 w-96 -left-48 -top-80  bg-brand-blueShadow/50"
         />
         <div
           style={{
             boxShadow: "rgb(27 120 222 / 60%) 0px 0px 100px 60px",
           }}
-          className="absolute  rounded-full opacity-40 h-96 w-96 -right-40 -bottom-48  bg-brand-blueShadow/50"
+          className="absolute  rounded-full opacity-30 h-96 w-96 -right-40 -bottom-48 pointer-events-none bg-brand-blueShadow/50"
         />
         <GradLine />
 
-        <div className="flex justify-between items-start max-md:flex-col">
+        <div className="flex justify-between items-start mt-4 max-md:flex-col">
           <h3 className="text-2xl font-bold max-md:mb-6">What is GPT-3</h3>
           <p className="text-base md:w-2/3  text-brand-textBlue">
             We so opinion friends me message as delight. Whole front do of plate
@@ -43,17 +43,17 @@ const About = () => {
         <div className="mt-16 flex justify-between items-start max-md:flex-col max-lg:text-sm">
           <div className="md:mr-6 max-md:mb-12">
             <GradLine />
-            <h4 className="text-xl lg:text-2xl font-semibold lg:font-bold">
+            <h4 className="text-xl mt-4 lg:text-2xl font-semibold lg:font-bold">
               Chatbots
             </h4>
-            <p className="text-brand-textBlue md:mt-6 mt-4 ">
+            <p className="text-brand-textBlue text-base md:mt-6 mt-4 ">
               We so opinion friends me message as delight. Whole front do of
               plate heard oh ought.
             </p>
           </div>
           <div className="md:mr-6 max-md:mb-12">
             <GradLine />
-            <h4 className="text-xl lg:text-2xl font-semibold lg:font-bold">
+            <h4 className="text-xl mt-4 lg:text-2xl font-semibold lg:font-bold">
               Knowledgebase
             </h4>
             <p className="text-brand-textBlue md:mt-6 mt-4">
@@ -63,7 +63,7 @@ const About = () => {
           </div>
           <div className="md:ml-6">
             <GradLine />
-            <h4 className="text-xl lg:text-2xl font-semibold lg:font-bold">
+            <h4 className="text-xl mt-4 lg:text-2xl font-semibold lg:font-bold">
               Education
             </h4>
             <p className="text-brand-textBlue md:mt-6 mt-4">
