cfd1fce196a89177c0cdd19af0ba5c7bdf4cf516
Reza Sadeghzadeh <reza_sadeghzadeh01@yahoo.com>
2022-12-08 12:58:28 +0330

    fix: header

---
+++ components/Header.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="gp3-container py-6">
+      <div className="flex justify-between items-center">
+        <div className="flex justify-start items-center w-2/3">
+          <Link href={"/"} className="inline-block min-w-fit">
+            <h1 className="text-xl Manropebold pr-16 min-w-fit">GPT-3</h1>
+          </Link>
+          <div className="w-4/5 max-lg:hidden flex justify-between items-center text-base px-6">
+            <Link href={"/"}>Home</Link>
+            <Link href={"/"}>What is GPT?</Link>
+            <Link href={"/"}>Open AI</Link>
+            <Link href={"/"}>Case Studies</Link>
+            <Link href={"/"}>Library</Link>
+          </div>
+        </div>
+        <div className="flex justify-center items-center max-md:hidden">
+          <button className="md:px-8 md:py-4 md:text-base py-2 px-6 text-sm min-w-fit mr-4">
+            Sign In
+          </button>
+          <button className="md:px-8 md:py-4 md:text-base py-4 px-6 text-sm min-w-fit bg-brand-orange rounded-lg">
+            Sign In
+          </button>
+        </div>
+        <div className="humberger md:ml-12 lg:hidden"></div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
